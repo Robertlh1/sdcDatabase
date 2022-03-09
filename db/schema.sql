765a17c1ceb2b33c3efa3ffc0d3b575facfd8983
Robertlh1 <robertlh1@DESKTOP-5JGUGOE.localdomain>
2022-03-09 08:33:24 -0700

    optimized schema

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ helpful INT
 CREATE INDEX questionsIndex ON questions (product_id);
 
 COPY questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
-FROM '/home/ubuntu/sdcDatabase/db/csv/questions.csv'
+FROM '/home/ubuntu/sdcDatabase/db/csv/questions_date_adjusted.csv'
 DELIMITER ','
 HEADER CSV;
 
@@ -39,7 +39,7 @@ CONSTRAINT fk_question
 CREATE INDEX answersIndex ON answers (question_id);
 
 COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
-FROM '/home/ubuntu/sdcDatabase/db/csv/answers.csv'
+FROM '/home/ubuntu/sdcDatabase/db/csv/answers_date_adjusted.csv'
 DELIMITER ','
 HEADER CSV;
 
@@ -60,64 +60,3 @@ COPY answers_photos(id, answer_id, url)
 FROM '/home/ubuntu/sdcDatabase/db/csv/answers_photos.csv'
 DELIMITER ','
 HEADER CSV;
-
-CREATE TABLE tempquestions(
-id SERIAL PRIMARY KEY,
-product_id INT,
-body VARCHAR (1000),
-date_written VARCHAR(24),
-asker_name VARCHAR(60),
-asker_email VARCHAR(60),
-reported BOOLEAN,
-helpful INT,
-);
-
-COPY tempquestions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
-FROM '/home/ubuntu/sdcDatabase/db/csv/questions.csv'
-DELIMITER ','
-HEADER CSV;
-
-UPDATE questions
-SET date_written = TO_TIMESTAMP(tempquestions.date_written:: double precision / 1000)::timestamp with time zone at time zone 'Etc/UTC'
-FROM tempquestions
-WHERE questions.id = tempquestions.id;
-
-DROP TABLE tempquestions;
-
-UPDATE questions
-SET date_written = REPLACE(date_written, ' ', 'T');
-
-UPDATE questions
-SET date_written = CONCAT(date_written, 'Z');
-
-CREATE TABLE tempanswers(
-id SERIAL PRIMARY KEY,
-question_id INT,
-body VARCHAR (1000),
-date_written VARCHAR(24),
-answerer_name VARCHAR(60),
-answerer_email VARCHAR(60),
-reported BOOLEAN,
-helpful INT,
-CONSTRAINT fk_question
-  FOREIGN KEY(question_id)
-    REFERENCES questions(id)
-);
-
-COPY tempanswers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
-FROM '/home/ubuntu/sdcDatabase/db/csv/answers.csv'
-DELIMITER ','
-HEADER CSV;
-
-UPDATE answers
-SET date_written = TO_TIMESTAMP(tempanswers.date_written:: double precision / 1000)::timestamp with time zone at time zone 'Etc/UTC'
-FROM tempanswers
-WHERE answers.id = tempanswers.id;
-
-DROP TABLE tempanswers;
-
-UPDATE answers
-SET date_written = REPLACE(date_written, ' ', 'T');
-
-UPDATE answers
-SET date_written = CONCAT(date_written, 'Z');
